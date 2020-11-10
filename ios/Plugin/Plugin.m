63ce9bc2503fbe85ffa1e641cfdc540893812a2a
Todd Miller <todd@zifty.com>
2020-11-10 09:04:01 -0500

    iOS update

--- ios/Plugin/Plugin.m
+++ ios/Plugin/Plugin.m
@@ -5,4 +5,8 @@
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(TreadsGps, "TreadsGps",
            CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(checkStatus, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(requestIgnoreBatteryOptimization, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(requestIgnoreDataSaver, CAPPluginReturnPromise);
 )
+
